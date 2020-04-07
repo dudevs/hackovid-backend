26dd2a08ce268d4de92276aae16cf2b58694bfbf
fireball787b <fireball787b@gmail.com>
2020-04-07 16:12:49 +0200

    Create project structure

---
+++ backend/Controllers/InfoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Info> Get()
+        {
+            Info info = new Info
+            {
+                version = "v1",
+                databaseConnectionEstablished = false,
+                time = DateTime.Now
+            };
+
+            return Ok(info);
+        }
+    }
+}
---
+++ backend/Models/Info.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace backend.Models
+{
+    public class Info
+    {
+        public string version { get; set; }
+        public bool databaseConnectionEstablished { get; set; }
+        public DateTime time { get; set; }
+
+        public Info()
+        {
+        }
+    }
+}
