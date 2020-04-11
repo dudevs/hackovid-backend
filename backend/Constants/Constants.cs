68091b03e72ed97b1aae778a183085b54c6afd2b
fireball787b <fireball787b@gmail.com>
2020-04-12 00:24:18 +0200

    distance search adjusted

--- backend/Constants/Constants.cs
+++ backend/Constants/Constants.cs
@@ -12,7 +12,7 @@
         public const double MIN_LONGITUDE = 180;
         public const double MAX_LONGITUDE = -180;
 
-        public const int MAX_DISTANCE_METERS = 4000;
+        public const int MAX_DISTANCE_METERS = 1400;
 
         //TODO: moure a variables d'entorn
         public const string LOCATION_IQ_API_KEY = "09c9f9f19650d2";
