03727a9717a84b5e236c376a970485b800172f94
fireball787b <fireball787b@gmail.com>
2020-04-11 21:23:47 +0200

    distance updated

--- backend/Constants/Constants.cs
+++ backend/Constants/Constants.cs
@@ -12,7 +12,7 @@
         public const double MIN_LONGITUDE = 180;
         public const double MAX_LONGITUDE = -180;
 
-        public const int MAX_DISTANCE_METERS = 1000;
+        public const int MAX_DISTANCE_METERS = 4000;
 
         public const string LOCATION_IQ_API_KEY = "09c9f9f19650d2";
         public const string GOOGLE_MAPS_API_KEY = "AIzaSyDOSNzdaR7KH38DS7SW105Lsmmbfr3WQm4";
